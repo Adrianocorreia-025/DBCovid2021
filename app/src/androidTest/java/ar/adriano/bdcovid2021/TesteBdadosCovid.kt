bbe6f08fd603f206a9103a7144d3840773bef96d
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-06-03 17:59:04 +0100

    Criada funcao para Converter Funcao (toContent)

--- app/src/androidTest/java/ar/adriano/bdcovid2021/TesteBdadosCovid.kt
+++ app/src/androidTest/java/ar/adriano/bdcovid2021/TesteBdadosCovid.kt
@@ -2,11 +2,13 @@ package ar.adriano.bdcovid2021
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import ar.adriano.bdasecovid.TabelaEnfermeiro
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,12 +19,41 @@ import org.junit.Assert.*
 class TesteBdadosCovid {
     private fun getAppContext() = InstrumentationRegistry.getInstrumentation().targetContext
 
+    private fun getBdPacientesOpenHelper() = BdPacientesOpenHelper(getAppContext())
+
+ //   private fun getTabelaEnfermeiro(SQLiteDatabase db) = TabelaEnfermeiro(db)
+
+
+ @Before
+ fun ApagarBDados(){
+
+     getAppContext().deleteDatabase(BdPacientesOpenHelper.NOME_BASE_DADOS)
+ }
+
+
     @Test
     fun ConsegueAbrirBaseDados(){
-        val openHelper = BdPacientesOpenHelper(getAppContext())
-        val db = openHelper.readableDatabase
+        val db = getBdPacientesOpenHelper().writableDatabase
         assert(db.isOpen)
         db.close()
 
     }
+
+
+    fun ConsegueInserirEnfermeiros(){
+
+        val db = getBdPacientesOpenHelper().writableDatabase
+        val tabelaEnfermeiro = TabelaEnfermeiro(db)
+
+      tabelaEnfermeiro.insert(Enfermeiro(
+          nome = "Nome",
+          sexo = "Sexo",
+          cidade = "Cidade"
+      ))
+
+        db.close()
+    }
+
+
+
 }
--- app/src/main/java/ar/adriano/bdcovid2021/Enfermeiro.kt
+++ app/src/main/java/ar/adriano/bdcovid2021/Enfermeiro.kt
@@ -1,6 +1,24 @@
 package ar.adriano.bdcovid2021
 
-class Enfermeiro (var nome:String, var sexo:String, var Contacto:Int,var dataTeste:Int, var cidade:String) {
+import android.content.ContentValues
+import androidx.core.content.contentValuesOf
+import ar.adriano.bdasecovid.TabelaEnfermeiro
+
+class Enfermeiro (var nome:String, var sexo:String, var Contacto:Int = -1 ,var dataTeste:Int=-1, var cidade:String) {
+
+fun toContentValues():ContentValues{
+   val valores = ContentValues()
+
+    valores.put(TabelaEnfermeiro.NOME_ENFERMEIRO,nome)
+    valores.put(TabelaEnfermeiro.SEXO,sexo)
+    valores.put(TabelaEnfermeiro.CONTACTO,Contacto)
+     valores.put(TabelaEnfermeiro.CIDADE,cidade)
+    valores.put(TabelaEnfermeiro.DATA_TESTE,dataTeste)
+
+
+    return  valores
+  }
+
 
 
 }
